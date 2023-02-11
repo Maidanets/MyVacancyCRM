808495a2986ab8a1d9b1fb066009ad54a062cfaf
Maidanets <dimonchyk.ua@gmail.com>
2023-02-11 19:43:02 +0200

    update db_processes.py

--- db_processes.py
+++ db_processes.py
@@ -1,22 +1,31 @@
 import sqlite3
 
 
-def select_info(query):
-    conn = sqlite3.connect('vacancy.db')
-    c = conn.cursor()
-    c.execute(query)
-    result = c.fetchall()
-    conn.close()
-    return result
+class Database:
+    def __enter__(self):
+        self.conn = sqlite3.connect('vacancy.db')
+        self.c = self.conn.cursor()
+        return self
 
+    def query(self, qry):
+        self.c.execute(qry)
+        result = self.c.fetchall()
+        return result
 
-def insert_info(table_name, data):
-    columns = ', '.join(data.keys())
-    placeholder = ':' + ', :'.join(data.keys())
-    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholder})"
-    conn = sqlite3.connect('vacancy.db')
-    c = conn.cursor()
-    c.execute(query, data)
-    conn.commit()
-    conn.close()
+    def insert(self, table_name, data):
+        columns = ', '.join(data.keys())
+        placeholder = ':' + ', :'.join(data.keys())
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholder})"
+        self.c.execute(query, data)
+        self.conn.commit()
+
+    def update(self, table_name, data, conditions):
+        line = ", ".join([f"{i}=:{i}" for i in data.keys()])
+        query = f"UPDATE {table_name} SET {line} WHERE {conditions}"
+        self.c.execute(query, data)
+        self.conn.commit()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.c.close()
+        self.conn.close()
 
