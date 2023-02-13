40a634277a5326bfbbeebe1bac73e09e1f3e1b28
Maidanets <dimonchyk.ua@gmail.com>
2023-02-14 00:04:58 +0200

    add db_alchemy.py

--- db_alchemy.py
+++ db_alchemy.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://postgres:password@localhost:5432/postgres')
+engine = create_engine('sqlite:///vacancy.db')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
