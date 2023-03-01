dd7ccc1f24020c88452ff7ab46eacd33b78241a0
Maidanets <dimonchyk.ua@gmail.com>
2023-03-02 00:24:23 +0200

    update alembic

---
+++ email_lib.py
@@ -0,0 +1,18 @@
+import os
+import smtplib, ssl
+
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "dimonchyk.ua@gmail.com"  # Enter your address
+receiver_email = "dimonchyk.ua+reciever@gmail.com"  # Enter receiver address
+password = os.environ.get('EMAIL_PASSWORD')
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
