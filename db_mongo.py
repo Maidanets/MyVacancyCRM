06039013bc855e3fb0f79e24aaa5711c18c4390d
Maidanets <dimonchyk.ua@gmail.com>
2023-04-13 01:04:58 +0300

    add db_mongo.py

---
+++ db_mongo.py
@@ -0,0 +1,8 @@
+import pymongo
+
+
+def init_db_mongo():
+    client = pymongo.MongoClient('mongodb://root:example@127.0.0.1:27017/')
+    database = client['crm_db']
+    contacts_collection = database['contacts']
+    return contacts_collection
